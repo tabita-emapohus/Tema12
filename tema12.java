56c3d6b100104cdf9a4abbad6ec08adea405129e
tabita-emapohus <161648276+tabita-emapohus@users.noreply.github.com>
2024-04-16 20:40:27 +0300

    Update tema12.java

---
+++ tema12.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class List {
+
+	public static void main(String[] args) {
+
+	       ArrayList<String> list = new ArrayList<>();
+	       list.add("world");
+	       list.add("hello");
+	       System.out.println(list);
+	       
+	       
+	       ArrayList < String > list2 = new ArrayList<>();
+	        list2.add("hello");;
+	       System.out.println(list2);
+	       
+	       
+	       ArrayList < String > list3 = new ArrayList<>();
+	       System.out.println(list3);
+	}
+
+}
